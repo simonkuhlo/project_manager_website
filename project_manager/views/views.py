f7c6cdafdb8beb6cf0df4cd0d598e737a05b05bf
simonkuhlo <siga241@gmail.com>
2023-12-18 12:51:56 +0100

    add views

--- project_manager/views/views.py
+++ project_manager/views/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponseRedirect
 from .. import models
 
 
+def homescreen(request):
+    return render(request, 'project_manager/home.html')
+
+def project_list(request):
+    projects = models.Project.objects.all()
+    ctx = {"project_list" : projects}
+    return render(request, 'project_manager/project_list.html', ctx)
+
 def project_details(request, id):
     project = models.Project.objects.get(id=id)
     ctx = {"project" : project}
