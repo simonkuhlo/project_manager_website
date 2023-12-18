4fbbbbcbd1bbdddf02a7c086e43ea171a07e3727
simonkuhlo <siga241@gmail.com>
2023-12-18 14:08:30 +0100

    add homescreen

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', views.formviews.upload_form, name = "upload_form"),
+    path('home', views.views.homescreen, name = "home"),
     path('new_project', views.formviews.project_form, name = "project_form"),
     path('project_list', views.views.project_list, name = "project_list"),
     path('project:<str:pk>/', views.views.project_details, name = "project_list"),
